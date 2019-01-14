fe8ccdec12d8dc70e049a3bcf214040155366195
Frederik Bolding <frederik.bolding@gmail.com>
2019-01-14 15:06:55 +0100

    Disabled prefix numbering

--- backup.py
+++ backup.py
@@ -2,4 +2,4 @@ from pytube import Playlist
 
 playlist = Playlist("https://www.youtube.com/playlist?list=UUdqmP4axeI1hNmX20aZsOwg", suppress_exception = True)
 
-playlist.download_all("backup", skip_existing = True)
+playlist.download_all("backup", skip_existing = True, prefix_number = False)
